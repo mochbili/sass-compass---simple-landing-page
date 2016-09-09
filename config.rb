457ab650f7a3031a65c5d37125fd8626e009ac14
mochbili <mochbili@gmail.com>
2016-09-09 21:33:36 +0700

    initial commit

---
+++ config.rb
@@ -0,0 +1,22 @@
+require 'compass/import-once/activate'
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :expanded
+sourcemap = false
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = false
+sass_options = {:debug_info=>false}
+
+line_comments = false
+
+preferred_syntax = :scss
